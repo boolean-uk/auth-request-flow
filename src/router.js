0da39dddaccd1e9b1351c39d63419c92d561adb1
patrik <patrik.ellini@outlook.com>
2022-12-05 11:31:36 +0000

    credentials completed

--- src/router.js
+++ src/router.js
@@ -1,25 +1,35 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
-
+router.post("/login", (req, res) => {
+    const {username, password} = req.body
+    if(username !== mockUser.username || password !== mockUser.password){
+        return "invalid credentials"
+    }
+  const token = jwt.sign({ username: "authguy" }, "mysecret");
+  res.json(token);
 });
 
-router.get('/profile', (req, res) => {
-  
+router.get("/profile", (req, res) => {
+  try {
+    const auth = req.get("Authorization");
+    jwt.verify(auth, "mysecret")
+    return res.json(mockUser.profile)
+  } catch (e) {
+    return "custom error"
+  }
 });
-
 
 module.exports = router;
