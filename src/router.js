121dda28f5eb5a968a87f080f0f66be2cc251296
akibria12 <ahkibria@hotmail.co.uk>
2023-01-23 18:04:25 +0000

    core completed

--- src/router.js
+++ src/router.js
@@ -1,25 +1,36 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-
+const express = require("express");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const router = express.Router();
 
+const secret = `${process.env.JWT_SECRET}`;
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
-
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username !== mockUser.username || password !== mockUser.password) {
+    res.status(400).json({ error: "Invalid username or password" });
+  }
+  const token = jwt.sign(req.body, secret);
+  res.json(token);
 });
 
-router.get('/profile', (req, res) => {
-  
+router.get("/profile", (req, res) => {
+  try {
+    const auth = req.headers.authorization;
+    jwt.verify(auth, secret);
+    return res.json({ profile: mockUser.profile });
+  } catch (e) {
+    res.status(401).json({ message: "Invalid token" });
+  }
 });
-
 
 module.exports = router;
