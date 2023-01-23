daaed2f88c91f32f0fd8ab00ba744ec5b3049e11
fatima <fxtimaahmed14@gmail.com>
2023-01-23 16:00:05 +0000

    req 1 done

--- src/router.js
+++ src/router.js
@@ -15,8 +15,7 @@ const mockUser = {
 };
 
 router.post('/login', (req, res) => {
-    const { username } = req.body
-    const token = jwt.sign(username, secret)
+    const token = jwt.sign(mockUser.username, secret)
     res.json({ token })
 });
 
