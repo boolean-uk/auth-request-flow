ee64a108d2afef69ff0370e9a0ef6033e99f572f
Luciano Simoni <lucianosimonipersonal@gmail.com>
2023-01-23 15:22:55 +0000

    true third done

--- src/router.js
+++ src/router.js
@@ -16,7 +16,23 @@ const mockUser = {
 const secret = process.env.JWT_SECRET;
 
 router.post("/login", (req, res) => {
-  res.json(jwt.sign(req.body, secret));
+  // TODO: CHECK FOR MOCHUSER MATCH
+  const { username, password, profile } = req.body;
+  try {
+    if (
+      username !== mockUser.username ||
+      password !== mockUser.password ||
+      profile.firstName !== mockUser.profile.firstName ||
+      profile.lastName !== mockUser.profile.lastName ||
+      profile.age !== mockUser.profile.age
+    )
+      throw new Error("Not the same guy");
+
+    res.json(jwt.sign(req.body, secret));
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+  // TODO: CREATE AND SEND BACK TOKEN
 });
 
 router.get("/profile", (req, res) => {
