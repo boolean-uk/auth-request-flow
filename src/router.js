fefd4cfd35e71efcdb7b1739f95432aad761f978
Dave <Daverevell491@gmail.com>
2023-01-23 14:15:28 +0000

    Req. 1 complete

--- src/router.js
+++ src/router.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+	username: "authguy",
+	password: "mypassword",
+	profile: {
+		firstName: "Chris",
+		lastName: "Wolstenholme",
+		age: 43,
+	},
 };
 
-router.post('/login', (req, res) => {
+const secret = process.env.JWT_SECRET;
 
+router.post("/login", (req, res) => {
+	res.json(jwt.sign(req.body, secret));
 });
 
-router.get('/profile', (req, res) => {
-  
-});
-
+router.get("/profile", (req, res) => {});
 
 module.exports = router;
