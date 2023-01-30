62f492f3d149d063135fe9bfbd184c8e0f9a3c64
akibria12 <ahkibria@hotmail.co.uk>
2023-01-30 16:40:22 +0000

    bug fixxed

--- src/router.js
+++ src/router.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const router = express.Router();
 
-const secret = `${process.env.JWT_SECRET}`;
+const secret = process.env.JWT_SECRET;
+
 const mockUser = {
   username: "authguy",
   password: "mypassword",
@@ -19,14 +20,15 @@ router.post("/login", (req, res) => {
   if (username !== mockUser.username || password !== mockUser.password) {
     res.status(400).json({ error: "Invalid username or password" });
   }
-  const token = jwt.sign(req.body, secret);
-  res.json(token);
+  const token = jwt.sign({ username }, secret);
+  res.json({ token });
 });
 
 router.get("/profile", (req, res) => {
   try {
-    const auth = req.headers.authorization;
-    jwt.verify(auth, secret);
+    let bearer = req.headers.authorization;
+    bearer = bearer.replace("Bearer ", "");
+    jwt.verify(bearer, secret);
     return res.json({ profile: mockUser.profile });
   } catch (e) {
     res.status(401).json({ message: "Invalid token" });
