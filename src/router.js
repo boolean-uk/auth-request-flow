60605a77e29a6cd012ef435f250d99ad3e41b58b
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-03-29 17:54:05 +0100

    Requirements 1 & 2 done

--- src/router.js
+++ src/router.js
@@ -1,25 +1,34 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
+const secret = process.env.JWT_SECRET;
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
+  const token = jwt.sign({ username: req.body.username }, secret);
 
+  res.json({ token });
 });
 
-router.get('/profile', (req, res) => {
-  
+router.get("/profile", (req, res) => {
+  const tokenToVerify = req.headers.authorization.split(" ")[1];
+  //   console.log(req.headers.authorization.split(" ")[1]);
+  try {
+    const decoded = jwt.verify(tokenToVerify, secret);
+    res.json({ profile: mockUser.profile });
+  } catch (error) {
+    res.status(401).json({ error: "Not authorized!" });
+  }
 });
-
 
 module.exports = router;
