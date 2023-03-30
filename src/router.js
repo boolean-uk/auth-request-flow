3cba1d4cca96491e55827baf79e971f4cb29e912
Asiyeyurtkuran <asiyeyurtkuran@gmail.com>
2023-03-30 19:01:32 +0100

    fixed credential

--- src/router.js
+++ src/router.js
@@ -4,30 +4,32 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: 'authguy',
+  password: 'mypassword',
+  profile: {
+    firstName: 'Chris',
+    lastName: 'Wolstenholme',
+    age: 43
+  }
 };
 
 router.post('/login', (req, res) => {
-    const mockUser = req.body;
-    try{
-        if(username === !mockUser.username ||
-           password === !mockUser.password ||
-           profile.firstName === !mockUser.profile.firstName ||
-           profile.lastName === !mockUser.profile.lastName ||
-           profile.age === !mockUser.profile.age){
-            throw new Error('Invalid credentials');
-           }
-    } catch (error) {
-        return res.status(401).json({ error: error})
+  const mockUser = req.body;
+  try {
+    if (username === !mockUser.username ||
+      password === !mockUser.password) {
+      throw new Error('Invalid credentials');
+    } else {
+      const token = jwt.sign({ username: mockUser.username }, process.env.JWT_SECRET)
+      res.json({ token })
     }
+  } catch (error) {
+    return res.status(401).json({ error: error })
+  }
 
 });
+
+
 
 router.get('/profile', (req, res) => {
   //extract the JWT from headers
@@ -35,13 +37,13 @@ router.get('/profile', (req, res) => {
   // if valid we return the users profile data
   console.log("PROFILE", req.headers);
   const token = req.headers.authorization.split(" ")[1]
-  try{
+  try {
     jwt.verify(token, process.env.JWT_SECRET)
-    return res.status(200).json({profile: mockUser.profile})
+    return res.status(200).json({ profile: mockUser.profile })
   } catch (e) {
-    return res.status(401).json({ error: e})
+    return res.status(401).json({ error: e })
   }
-  
+
 
 });
 
