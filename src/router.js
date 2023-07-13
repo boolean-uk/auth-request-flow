f463cc73fb7fcfd3732d1a2065e94f1364e942d4
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-07-13 14:07:10 +0200

    first two reqs finished

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET
 
 const router = express.Router();
 
@@ -15,9 +16,26 @@ const mockUser = {
 
 router.post('/login', (req, res) => {
 
+    const token = jwt.sign({payload: mockUser.username}, secret)
+
+    res.send({token})
+
 });
 
 router.get('/profile', (req, res) => {
+    const token = req.header("Authorization")
+    console.log(token)
+
+
+    try {
+        const auth = jwt.verify(token, secret)
+        console.log(auth)
+        res.send({profile: mockUser.profile})
+    } catch (e) {
+       
+        throw new Error("Incorrect token")
+    }
+      
   
 });
 
