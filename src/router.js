cbfb1f932953efe1e8e9c1ce43af9516f612e2dc
carolina arruda <carolinacalouroarruda@gmail.com>
2023-07-13 17:34:05 +0200

    finished core

--- src/router.js
+++ src/router.js
@@ -21,23 +21,27 @@ const createToken = (payload, secret) => {
 };
 
 router.post("/login", (req, res) => {
-  const { username } = req.body;
-  const myToken = createToken(username, secret);
-  res.status(200).json({ success: myToken });
+  const { username, password } = req.body;
+  if (username === "authguy" && password === "mypassword") {
+    const payload = { username, password };
+    const myToken = createToken(payload, secret);
+    res.status(200).json({ status: "success", data: {"token": myToken, user: mockUser.profile} });
+  } else {
+    return res.status(404).send("Your email or password don't exist");
+  }
 });
 
 router.get("/profile", (req, res) => {
-  const my_token = req.header("my_token")
+  const my_token = req.header("my_token");
 
   try {
-    const decoded = jwt.verify(my_token, secret)
-    console.log(decoded)
-    return res.send(mockUser.profile)
+    const decoded = jwt.verify(my_token, secret);
+    console.log(decoded);
+    return res.send(mockUser.profile);
   } catch (error) {
-    console.log("invalid token")
-    return res.status(404).send("Wrong token")
+    console.log("invalid token");
+    return res.status(404).send("Wrong token");
   }
-
 });
 
 module.exports = router;
