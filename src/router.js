94779c1a19cc71bfa8c76152296860c47246e621
carolina arruda <carolinacalouroarruda@gmail.com>
2023-07-13 16:53:50 +0200

    finished login

--- src/router.js
+++ src/router.js
@@ -1,25 +1,31 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { token } = require("morgan");
+const secret = process.env.JWT_TOKEN;
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
+const createToken = (payload, secret) => {
+  const token = jwt.sign(payload, secret);
+  return token;
+};
 
+router.post("/login", (req, res) => {
+  const { username } = req.body;
+  const myToken = createToken(username, secret);
+  res.status(200).json({ success: myToken });
 });
 
-router.get('/profile', (req, res) => {
-  
-});
-
+router.get("/profile", (req, res) => {});
 
 module.exports = router;
