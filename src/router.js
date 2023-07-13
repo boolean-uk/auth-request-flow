838181b5f5995d9d230c9ac78b327c42d0ac2135
Mantas Navickis <navickis@gmail.com>
2023-07-13 16:59:05 +0200

    ex 1,2 done

--- src/router.js
+++ src/router.js
@@ -1,25 +1,37 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const secret = process.env.JWT_SECRET
 
-const router = express.Router();
+const router = express.Router()
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
-};
+  username: 'authguy',
+  password: 'mypassword',
+  profile: {
+    firstName: 'Chris',
+    lastName: 'Wolstenholme',
+    age: 43
+  }
+}
 
 router.post('/login', (req, res) => {
-
-});
+  const { username, password } = req.body
+  if (username === mockUser.username && password === mockUser.password) {
+    res.status(201).json(jwt.sign({ username }, secret))
+  } else {
+    res.status(500).json('Error')
+  }
+})
 
 router.get('/profile', (req, res) => {
-  
-});
+  const tokenfromAuth = req.header('authorization')
+  const token = tokenfromAuth.split(" ")[1]
+  try {
+    jwt.verify(token, secret)
+    return res.json({ user: mockUser.profile })
+  } catch (error) {
+    res.status(500).json({ error: error })
+  }
+})
 
-
-module.exports = router;
+module.exports = router
