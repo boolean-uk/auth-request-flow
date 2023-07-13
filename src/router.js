ba0d561116d9f4d7bceae92f449303e448c7a82a
gid-ctrl <gideonusenbor@yahoo.co.uk>
2023-07-13 14:09:59 +0100

    requirements completed

--- src/router.js
+++ src/router.js
@@ -1,25 +1,38 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
-
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username === mockUser.username && password === mockUser.password) {
+    const payload = { username: mockUser.username };
+    const token = jwt.sign(payload, secret);
+    res.json({ token });
+  } else {
+    res.status(400).json({ error: "Wrong username and password entered" });
+  }
 });
 
-router.get('/profile', (req, res) => {
-  
+router.get("/profile", (req, res) => {
+  const payloadArray = req.headers.authorization.split(" ");
+  const payload = payloadArray[1];
+
+  try {
+    jwt.verify(payload, secret);
+    res.json({ profile: mockUser.profile });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
-
-
 module.exports = router;
