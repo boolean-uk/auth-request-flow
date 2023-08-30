abfda920c85e5f509d493d4947fd396d240f235d
davidbrockway <dav.brock@protonmail.com>
2023-08-30 20:05:54 +0100

    Finished Code

--- src/router.js
+++ src/router.js
@@ -14,12 +14,36 @@ const mockUser = {
 };
 
 router.post('/login', (req, res) => {
+    const { username, password } = req.body;
 
+    if (username === mockUser.username && password === mockUser.password) {
+        const secret = process.env.JWT_SECRET;
+        const payload = {
+            username: mockUser.username
+        };
+        const token = jwt.sign(payload, secret);
+        res.json({ token });
+    } else {
+        res.status(401).json({ error: 'Invalid credentials' });
+    }
 });
 
 router.get('/profile', (req, res) => {
-  
-});
+    const secret = process.env.JWT_SECRET;
+    const authHeader = req.headers.authorization;
 
+    if (!authHeader) {
+        return res.status(401).json({ error: 'No token provided' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, secret);
+        res.json(mockUser.profile);
+    } catch (error) {
+        res.status(401).json({ error: 'Invalid token' });
+    }
+});
 
 module.exports = router;
