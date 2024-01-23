6d4c9b6c98f6cefe070ef69e6a1e202279960784
ilham saleh <ilham2saleh@gmail.com>
2024-01-23 16:20:55 +0000

    Modify login controller function, check if username and paswrod provided match the value on mock user - complete

--- src/router.js
+++ src/router.js
@@ -16,14 +16,25 @@ const mockUser = {
 };
 
 router.post("/login", (req, res) => {
-  //   const payload = { username: mockUser.username };
-  const payload = { username: req.body.username };
+  const { username, password } = req.body;
 
-  try {
-    const token = jwt.sign(payload, secret);
-    return res.status(201).json({ token: token });
-  } catch (error) {
-    res.status(401).json({ error: error.message });
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Both username and password are required" });
+  }
+
+  if (username === mockUser.username && password === mockUser.password) {
+    const payload = { username: req.body.username };
+
+    try {
+      const token = jwt.sign(payload, secret);
+      return res.status(201).json({ token: token });
+    } catch (error) {
+      res.status(500).json({ error: "Error occured" });
+    }
+  } else {
+    return res.status(401).json({ error: "Invalid credentials" });
   }
 });
 
