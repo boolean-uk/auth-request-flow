ac97951ab8774c15630fdb55b7ce4e7087a83a4e
Pierluigi <pierluigi.capirci89@gmail.com>
2024-01-23 15:59:07 +0000

    token created and sent to client

--- src/router.js
+++ src/router.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
-
+router.post("/login", (req, res) => {
+  const token = jwt.sign(mockUser.username, secret);
+  res.json({ token });
 });
 
-router.get('/profile', (req, res) => {
-  
-});
-
+router.get("/profile", (req, res) => {});
 
 module.exports = router;
