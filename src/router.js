1b36c551c53a3f80160e0d503753e5e879d95f40
ilham saleh <ilham2saleh@gmail.com>
2024-01-23 14:18:03 +0000

    Requirement 1 - Login --> Get the token as the response

--- src/router.js
+++ src/router.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const secret = process.env.JWT_SECRET;
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
+  const payload = { username: mockUser.username };
+  //   const payload = req.headers.payload;
 
+  const token = jwt.sign(payload, secret);
+  return res.status(201).json({ token: token });
 });
 
-router.get('/profile', (req, res) => {
-  
-});
-
+router.get("/profile", (req, res) => {});
 
 module.exports = router;
