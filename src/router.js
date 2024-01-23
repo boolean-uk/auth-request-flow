4c343fce684b9c582625baecd55460576e2c026c
Lukas Dembicki <lad_777@hotmail.com>
2024-01-23 15:12:04 +0000

    FEAT: /login now checks credentials.

--- src/router.js
+++ src/router.js
@@ -15,24 +15,52 @@ const mockUser = {
 };
 
 router.post("/login", (req, res) => {
-    console.log(secret)
-    const payload = req.headers.payload;
+    console.log(secret);
+    const payload = req.body;
+    const username = req.body.username;
+    const password = req.body.password;
 
     function createToken(payload, secret) {
         const token = jwt.sign(payload, secret);
         return token;
     }
 
-    try {
-        const token = createToken(payload, secret);
-        res.status(201).json({ token: token });
-    } catch (err) {
-        res.status(400).json({
-            error: err.message,
-        });
+    if (password === mockUser.password && username === mockUser.username) {
+        try {
+            const token = createToken(payload, secret);
+            res.status(201).json({ token: token });
+        } catch (err) {
+            res.status(400).json({
+                error: err.message,
+            });
+        }
     }
+    res.status(401).json({ message: "username or password does not match" });
 });
 
-router.get("/profile", (req, res) => {});
+
+
+router.get("/profile", (req, res) => {
+
+    function verifyToken(token, secret) {
+        try {
+            const verifyAToken = jwt.verify(token, secret);
+            return verifyAToken;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+    }
+
+    const tokenWithBearer = req.headers.authorization.split(" ");
+    const token = tokenWithBearer[1];
+
+    try {
+        const validToken = verifyToken(token, secret);
+        res.status(202).json({ user: validToken });
+    } catch (err) {
+        res.status(401).json({ error: "Authorization failed" });
+    }
+});
 
 module.exports = router;
