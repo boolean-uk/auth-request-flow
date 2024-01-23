d4ec760faf11e026d23a07e1e504bcb1ada0accd
callumhayden <callum.hayden@gmail.com>
2024-01-23 13:29:12 +0000

    requirement one complete

--- src/router.js
+++ src/router.js
@@ -1,25 +1,37 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
+function createToken() {
+    try {
+      return jwt.sign({ username: mockUser.username }, secret);
+    } catch (error) {
+      console.error("Error creating token:", error);
+      return null;
+    }
+  }
 
+router.post("/login", (req, res) => {
+ const token = createToken()
+ if(token){
+    res.json({token})
+ } else {
+    res.status(500).json({message: 'Error creating token'})
+ }
 });
 
-router.get('/profile', (req, res) => {
-  
-});
-
+router.get("/profile", (req, res) => {});
 
 module.exports = router;
