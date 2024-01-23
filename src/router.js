b0341639aa3e836e1c3e95e673230bccd6ca774c
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-23 15:54:11 +0000

    done

--- src/router.js
+++ src/router.js
@@ -1,25 +1,43 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
+const secret = process.env.JWT_SECRET;
 
+router.post("/login", (req, res) => {
+  const { username, password } = req.body
+  if  (username !== mockUser.username || password !== mockUser.password) {
+    res.status(401).json({ error: "password/username is incorrect" });
+    return
+  }
+    const newJwt = jwt.sign({ username, password }, secret);
+    res.status(201).json({ newJwt });
 });
 
-router.get('/profile', (req, res) => {
-  
+router.get("/profile", (req, res) => {
+  const token = req.headers.authorization;
+  try {
+      jwt.verify(token, secret);
+      res.json({ profile: mockUser.profile });
+  } catch (e) {
+    if (e.message ===  "jwt malformed"){
+        res.status(401).json({ error: "authentication failed"})
+    }
+    if (e.message ===  "invalid signature"){
+        res.status(403).json({ error: "not authorised"})
+    }
+  }
 });
-
 
 module.exports = router;
