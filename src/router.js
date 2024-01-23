f72f9b4138418e6d7217af5d8f4e5dd5a8cb9c09
Lukas Dembicki <lad_777@hotmail.com>
2024-01-23 14:21:56 +0000

    FEAT: /login now creates and returns a token

--- src/router.js
+++ src/router.js
@@ -1,25 +1,38 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
+    username: "authguy",
+    password: "mypassword",
     profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+        firstName: "Chris",
+        lastName: "Wolstenholme",
+        age: 43,
+    },
 };
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
+    console.log(secret)
+    const payload = req.headers.payload;
 
+    function createToken(payload, secret) {
+        const token = jwt.sign(payload, secret);
+        return token;
+    }
+
+    try {
+        const token = createToken(payload, secret);
+        res.status(201).json({ token: token });
+    } catch (err) {
+        res.status(400).json({
+            error: err.message,
+        });
+    }
 });
 
-router.get('/profile', (req, res) => {
-  
-});
-
+router.get("/profile", (req, res) => {});
 
 module.exports = router;
