8cb7725807d15c251a9ab2eb488169c6e3be4b28
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-23 15:27:17 +0000

    exercise done

--- src/router.js
+++ src/router.js
@@ -1,25 +1,45 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
+const secret = process.env.JWT_SECRET;
+
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
+  const {username, password} = req.body
 
+  if (username !== mockUser.username) {
+    res.status(401).json({error: "username / password do not match"})
+    return
+  }
+
+  if (password !== mockUser.password) {
+    res.status(401).json({error: "username / password do not match"})
+    return
+  }
+
+  const payload = { username: mockUser.username };
+  const token = jwt.sign(payload, secret);
+  res.json({ token });
 });
 
-router.get('/profile', (req, res) => {
-  
+router.get("/profile", (req, res) => {
+  const [prefix, token] = req.headers.authorization.split(" ");
+  const verified = jwt.verify(token, secret);
+  if (verified) {
+    res.json(mockUser);
+  }
+  res.status(500);
 });
-
 
 module.exports = router;
