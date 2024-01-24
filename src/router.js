cadd313bed364a087423d479db3de65b4ce98584
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-24 00:57:59 +0000

    Add POST /login create the jwt token

--- src/router.js
+++ src/router.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const secret = process.env.JWT_SECRET
 
-const router = express.Router();
+const router = express.Router()
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
-};
+  username: 'authguy',
+  password: 'mypassword',
+  profile: {
+    firstName: 'Chris',
+    lastName: 'Wolstenholme',
+    age: 43
+  }
+}
 
 router.post('/login', (req, res) => {
+  try {
+    const jwtToken = jwt.sign({ username: mockUser.username }, secret)
 
-});
+    res.status(201).json({ jwtToken })
+  } catch (error) {
+    res.status(error.status ?? 500).json({ error: error.message })
+  }
+})
 
-router.get('/profile', (req, res) => {
-  
-});
+router.get('/profile', (req, res) => {})
 
-
-module.exports = router;
+module.exports = router
